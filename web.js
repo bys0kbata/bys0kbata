5a29642aa1a2e04351994422cf36b40e3d4e5443
bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2022-01-25 22:44:01 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,34 @@
+FileReader(String fileName) 
+FileReader(File file)
+FileReader(FileDescriptor fd)
+
+
 var express = require("express");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+import java.io.*;
+ 
+public class Program {
+ 
+    public static void main(String[] args) {
+        
+        try(FileReader reader = new FileReader("index.html")
+        {
+           // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+                 
+                System.out.print((char)c);
+            } 
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        }   
+    } 
+}
 });
 
 var port = process.env.PORT || 5000;
